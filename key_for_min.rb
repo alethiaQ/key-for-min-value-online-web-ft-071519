c90777725e0ffbe7db03daaacd1fa22b9d153d98
Alethia Quintero <alethiaq22@gmail.com>
2019-07-16 22:02:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 min_item = nil
 min_price = nil
   name_hash.each do |item, price|
-    if min_price = nil || price < min_price
+    if min_price == nil || price < min_price
       min_price = price
       min_item = item
     end
