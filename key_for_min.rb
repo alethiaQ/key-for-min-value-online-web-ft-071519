d7fcf56f881d51fb53d98f6bc974e9cc9dc78b2f
Alethia Quintero <alethiaq22@gmail.com>
2019-07-16 21:43:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  max = 0
+  min = 0
+  max_name = ""
+  min_name = ""
+  if name_hash.length > 0
+    name_hash.collect do |item, price|
+      if price > max
+        min = max && min_name = max_name if min < max
+        
+
+        max_name = item
+        max = price
+
 
 end
